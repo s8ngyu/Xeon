fe51f0f8dd6109055120611552c71ec951367872
peterprd <41620322+peterprd@users.noreply.github.com>
2019-03-31 11:53:07 +0900

    don't need this

--- xeonprefs/XENRootListController.h
+++ xeonprefs/XENRootListController.h
@@ -4,12 +4,6 @@
 #import <CepheiPrefs/HBAppearanceSettings.h>
 #import <Cephei/HBPreferences.h>
 #import <spawn.h>
-#import <libimagepicker.h>
-#import <LIPImageChooseCell.h>
-#import "XENCommon.h"
-#import "IconThemes.h"
-#import "XENGIFCommon.h"
-#import "GIFThemes.h"
 #import "XENCreditListController.h"
 #import "XENLeftListController.h"
 #import "XENRightListController.h"
